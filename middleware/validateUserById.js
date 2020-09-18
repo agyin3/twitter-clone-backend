fd6a820f2673815ad2ea389b6698f09cd0707768
Buddy Agyin <agyin3@gmail.com>
2020-09-18 03:02:52 -0600

    create custom middleware

---
+++ middleware/validateUserById.js
@@ -0,0 +1,24 @@
+const User = require('../data/models/User')
+
+module.exports = (req, res, next) => {
+    const { id } = req.params
+
+    try{
+        const user = await User.findById(id)
+
+        if(user){ 
+            res.locals.user = user
+            next()
+        }else{
+            throw new Error('Invalid user id provided')
+
+        }
+
+    }catch(err){
+        if(err.message.includes('Invalid')){
+            res.status(404).json({message: err.message, error: err})
+        }else {
+            res.status(500).json({message: 'There was an error retrieving the user from the database', error: err})
+        }
+    }
+}
